cee4d4554d51dd21fe29ba72a4ae600e544598a2
Armando Salazar <48894886+armandosalazar@users.noreply.github.com>
2024-09-27 14:17:04 -0600

    refactor: global styles and add font and style links to homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      fontFamily: {
-        body: ["Poppins", "sans-serif"],
-      },
     },
   },
   plugins: [],
