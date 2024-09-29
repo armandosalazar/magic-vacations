1e5a90a1583b20835a94c0ef113b63e7e580bcad
Armando Salazar <48894886+armandosalazar@users.noreply.github.com>
2024-09-29 02:48:19 -0600

    feat: add AOS library for animations

---
+++ src/components/Section.tsx
@@ -0,0 +1,9 @@
+const Section = ({ title }: { title: string }) => {
+  return (
+    <>
+      <h1>{title}</h1>
+    </>
+  );
+};
+
+export default Section;
---
+++ src/js/aos.js
@@ -0,0 +1,6 @@
+import AOS from 'aos';
+
+
+export default function aos() {
+    AOS.init();
+}
