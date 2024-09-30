cf102c051503ae10b2ad5bad81f708b8421cd1e3
Armando Salazar <48894886+armandosalazar@users.noreply.github.com>
2024-09-29 22:17:25 -0600

    refactor: Update Carousel and Header components
    - Add initialSlide property to Carousel component
    - Update styling and content in Carousel component
    - Add data-aos attribute to Header component

--- src/components/base/Carousel.jsx
+++ src/components/base/Carousel.jsx
@@ -13,6 +13,7 @@ export default function Carousel() {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    initialSlide: 0,
     responsive: [
       {
         breakpoint: 1024,
@@ -43,7 +44,11 @@ export default function Carousel() {
 
   return (
     <SliderComponent {...settings}>
-      <div className="rounded-3xl bg-white p-4">
+      <div className="relative flex justify-center rounded-3xl bg-white p-4">
+        <h2 className="absolute bottom-20 left-1/2 -translate-x-1/2 text-center font-bold text-white">
+          Viajes <br />
+          Nacionales
+        </h2>
         <img
           className="rounded-2xl"
           src="/images/banner-1.webp"
@@ -51,15 +56,27 @@ export default function Carousel() {
           srcset=""
         />
       </div>
-      <div className="rounded-3xl bg-white p-4">
+      <div className="relative rounded-3xl bg-white p-4">
+        <h2 className="absolute bottom-20 left-1/2 -translate-x-1/2 transform text-center font-bold text-white">
+          Viajes <br />
+          Internacionales
+        </h2>
         <img
           className="rounded-2xl"
           src="/images/banner-2.webp"
           alt=""
           srcset=""
         />
+        <button className="absolute bottom-1 left-1/2 z-50 -translate-x-1/2 rounded-3xl bg-[#626cec] px-4 py-2 text-sm text-white sm:flex">
+          Reservas
+        </button>
       </div>
-      <div className="rounded-3xl bg-white p-4">
+      <div className="relative rounded-3xl bg-white p-4">
+        <h2 className="absolute bottom-20 left-1/2 -translate-x-1/2 text-center font-bold text-white">
+          Coordinación
+          <br />
+          de eventos
+        </h2>
         <img
           className="rounded-2xl"
           src="/images/banner-3.webp"
